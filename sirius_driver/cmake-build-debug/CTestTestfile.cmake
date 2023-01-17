c9b385dbd4c8bc4cb7cb5960c4aa732894e2fb30
Simon Stapperfend <s.stapperfend@gmail.com>
2023-01-17 11:07:19 +0100

    added previous motor control files

---
+++ sirius_driver/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/max/catkin_ws/src/sirius/sirius_driver
+# Build directory: /home/max/catkin_ws/src/sirius/sirius_driver/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
