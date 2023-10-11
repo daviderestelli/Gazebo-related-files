3988b59d2857ec2d1432c17916bda78eaffdea3c
Ricardo Pena <r.pena@campus.tu-berlin.de>
2023-10-11 13:36:32 +0000

    Erc23

--- sirius_driver/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/max/catkin_ws/src/sirius/sirius_driver
-# Build directory: /home/max/catkin_ws/src/sirius/sirius_driver/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
